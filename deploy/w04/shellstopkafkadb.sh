b815739412a1750cd9dc7e1989d7e3b0823eb11a
wangxiaoqing <xiaoqingwang@126.com>
2018-05-02 16:41:14 +0800

    no message

--- deploy/w04/shellstopkafkadb.sh
+++ deploy/w04/shellstopkafkadb.sh
@@ -1,32 +1,32 @@
-docker-compose -f ./kafkadb111.yml down -d
-docker-compose -f ./kafkadb112.yml down -d
-docker-compose -f ./kafkadb113.yml down -d
-docker-compose -f ./kafkadb114.yml down -d
-docker-compose -f ./kafkadb115.yml down -d
-docker-compose -f ./kafkadb116.yml down -d
-docker-compose -f ./kafkadb117.yml down -d
-docker-compose -f ./kafkadb118.yml down -d
-docker-compose -f ./kafkadb211.yml down -d
-docker-compose -f ./kafkadb212.yml down -d
-docker-compose -f ./kafkadb213.yml down -d
-docker-compose -f ./kafkadb214.yml down -d
-docker-compose -f ./kafkadb215.yml down -d
-docker-compose -f ./kafkadb216.yml down -d
-docker-compose -f ./kafkadb217.yml down -d
-docker-compose -f ./kafkadb218.yml down -d
-docker-compose -f ./kafkadb311.yml down -d
-docker-compose -f ./kafkadb312.yml down -d
-docker-compose -f ./kafkadb313.yml down -d
-docker-compose -f ./kafkadb314.yml down -d
-docker-compose -f ./kafkadb315.yml down -d
-docker-compose -f ./kafkadb316.yml down -d
-docker-compose -f ./kafkadb317.yml down -d
-docker-compose -f ./kafkadb318.yml down -d
-docker-compose -f ./kafkadb411.yml down -d
-docker-compose -f ./kafkadb412.yml down -d
-docker-compose -f ./kafkadb413.yml down -d
-docker-compose -f ./kafkadb414.yml down -d
-docker-compose -f ./kafkadb415.yml down -d
-docker-compose -f ./kafkadb416.yml down -d
-docker-compose -f ./kafkadb417.yml down -d
-docker-compose -f ./kafkadb418.yml down -d
+docker-compose -f ./kafkadb111.yml down
+docker-compose -f ./kafkadb112.yml down
+docker-compose -f ./kafkadb113.yml down
+docker-compose -f ./kafkadb114.yml down
+docker-compose -f ./kafkadb115.yml down
+docker-compose -f ./kafkadb116.yml down
+docker-compose -f ./kafkadb117.yml down
+docker-compose -f ./kafkadb118.yml down
+docker-compose -f ./kafkadb211.yml down
+docker-compose -f ./kafkadb212.yml down
+docker-compose -f ./kafkadb213.yml down
+docker-compose -f ./kafkadb214.yml down
+docker-compose -f ./kafkadb215.yml down
+docker-compose -f ./kafkadb216.yml down
+docker-compose -f ./kafkadb217.yml down
+docker-compose -f ./kafkadb218.yml down
+docker-compose -f ./kafkadb311.yml down
+docker-compose -f ./kafkadb312.yml down
+docker-compose -f ./kafkadb313.yml down
+docker-compose -f ./kafkadb314.yml down
+docker-compose -f ./kafkadb315.yml down
+docker-compose -f ./kafkadb316.yml down
+docker-compose -f ./kafkadb317.yml down
+docker-compose -f ./kafkadb318.yml down
+docker-compose -f ./kafkadb411.yml down
+docker-compose -f ./kafkadb412.yml down
+docker-compose -f ./kafkadb413.yml down
+docker-compose -f ./kafkadb414.yml down
+docker-compose -f ./kafkadb415.yml down
+docker-compose -f ./kafkadb416.yml down
+docker-compose -f ./kafkadb417.yml down
+docker-compose -f ./kafkadb418.yml down
